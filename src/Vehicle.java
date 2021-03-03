8357b2499c12c3cb280da16df45c56884bc8aa4a
David Fontes <david.fontes@ubixploit.pt>
2021-03-03 22:33:28 +0000

    Add Vehicle class

---
+++ src/Vehicle.java
@@ -0,0 +1,19 @@
+public class Vehicle {
+
+    private String model;
+    private String plate;
+
+    public constructor(String model, String plate) {
+        this.model = model;
+        this.plate = plate;
+    }
+
+    public String getModel() {
+        return this.model;
+    }
+
+    public String getPlate() {
+        return this.plate;
+    }
+
+}
